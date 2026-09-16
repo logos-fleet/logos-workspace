ef8daa287fa9da9eee0ac1945ef32b62523f6813
Alex Jbanca <alexjb@status.im>
2026-09-17 01:49:54 +0300

    chore(workspace): pin logos-evm-railgun-module 47da834 (#213 review cleanup)

--- flake.nix
+++ flake.nix
@@ -412,7 +412,7 @@
       inputs.logos-module-builder.follows = "logos-module-builder";
     };
     logos-evm-railgun-module = {
-      url = "github:logos-fleet/logos-evm-railgun-module/5190b1c3945e6e21b64c22c958245eab014c5f18";
+      url = "github:logos-fleet/logos-evm-railgun-module/47da83402c708442202dd8705fd27de930f0c4df";
       inputs.eth_rpc_module.follows = "logos-evm-eth-rpc-module";
       inputs.keystore_module.follows = "logos-evm-keystore-module";
       inputs.logos-module-builder.follows = "logos-module-builder";
